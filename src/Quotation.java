a29488cae6441793c6c81e827a78f18f73df7aa3
MargoBB <115227124+MargoBB@users.noreply.github.com>
2023-01-30 04:47:31 +0200

    Update Quotation.java

--- src/Quotation.java
+++ src/Quotation.java
@@ -1,6 +1,9 @@
 public class Quotation {
-  String quote = "Welcome to Sun!";
+  String quote = "A man should have three keys: from the apartament, from the car and from the beer bottle. Finnish proverb.";
+  public String who = "~the man";
+  
   public void display() {
     System.out.println(quote);
+    System.out.println(who);
   }
 }
